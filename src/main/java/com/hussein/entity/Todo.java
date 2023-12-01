6410d1b3138a513fb0650c80563d3c945a219a18
221hussein <yuzinpream51@gmail.com>
2023-12-01 12:36:26 +0000

    adding validation in field Entity

--- src/main/java/com/hussein/entity/Todo.java
+++ src/main/java/com/hussein/entity/Todo.java
@@ -1,21 +1,89 @@
 package com.hussein.entity;
 
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Table(name = "todo_table")
-public class Todo extends AbstractEntity{
+public class Todo extends AbstractEntity {
+
+    @NotEmpty(message = "Task must not be empty")
+    @Size(min = 5, max = 100, message = "Task must be between 5 and 100 characters")
     private String task;
     private LocalDate dateCreated;
+    @NotNull(message = "Due date must not be empty")
+    @FutureOrPresent(message = "Due date must be present or in the future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+
     private boolean isCompleted;
     private boolean archived;
     private boolean reminder;
-    @ManyToOne(
 
-    )
+    @ManyToOne()
+    @JoinColumn(name ="todoUser_id")
     private TodoUser todoOwner;
 
+    public String getTask() {
+        return task;
+    }
 
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
+    }
+
+    public boolean isReminder() {
+        return reminder;
+    }
+
+    public void setReminder(boolean reminder) {
+        this.reminder = reminder;
+    }
+
+    public TodoUser getTodoOwner() {
+        return todoOwner;
+    }
+
+    public void setTodoOwner(TodoUser todoOwner) {
+        this.todoOwner = todoOwner;
+    }
 }
--- src/main/java/com/hussein/entity/TodoUser.java
+++ src/main/java/com/hussein/entity/TodoUser.java
@@ -1,19 +1,31 @@
 package com.hussein.entity;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
+import javax.validation.constraints.*;
 
 @Entity
-@Table(name = "todo_user_table")
 public class TodoUser extends AbstractEntity {
 
-    @Column(nullable = false)
+    @NotEmpty(message = "fullName must not be empty")
+    @Size(min=2 , max=50, message = "fullName must be between 2 and 50 characters")
     private String fullName;
 
     @Column(unique = true)
+    @NotEmpty(message = "email mustbe set")
+    @Email(message = "email should be valid")
     private String email;
+
+
+    @NotNull(message = "password must not be empty")
+    @Size(min=8 , max=100, message = "password must be between 8 and 50 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
+            message = "password must have at least one uppercase, one lowercase and one digit")
     private String password;
 
+
+//    @OneToMany
+//    private final Collection<Todo> todos =
+//            new ArrayList<>();
 
 
     public String getFullName() {
